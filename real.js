958bbbed0270c9035191f16f416b08401e0f8176
monaiem2021 <86136724+monaiem2021@users.noreply.github.com>
2021-06-21 10:41:21 -0700

    Update real.js

--- real.js
+++ real.js
@@ -159,6 +159,39 @@ firstList.style.marginTop = "100px";
 
 
 
+secondList.style.backgroundColor = "#150E56";
+secondList.style.marginTop = "100px";
+
+
+
+thirdList.style.backgroundColor = "#150E56";
+thirdList.style.marginTop = "100px";
+
+
+
+fourthList.style.backgroundColor = "#150E56";
+fourthList.style.marginTop = "100px";
+
+
+fifthList.style.backgroundColor = "#150E56";
+fifthList.style.marginTop = "100px";
+
+
+sixthList.style.backgroundColor = "#150E56";
+sixthList.style.marginTop = "100px";
+
+
+
+seventhList.style.backgroundColor = "#150E56";
+seventhList.style.marginTop = "100px";
+
+
+
+
+
+
+
+
 
 
 seventhList.style.backgroundColor = "#150E56";
